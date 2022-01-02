6ed9749f71e2c191c1786f35c18e8fb5b91a073d
Felix Wong <felixwongyuenlam@gmail.com>
2022-01-02 13:06:37 +0800

    Finish route animation

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="max-w-screen-lg h-96 mx-auto my-16 bg-primary bg-opacity-20"
+    class="max-w-screen-lg h-96 mx-auto bg-primary bg-opacity-20"
   ></header>
 </template>
 
